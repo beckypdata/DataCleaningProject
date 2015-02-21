0e6b8e728fcb1740f2853f3021c905693e683a62
Becky Portier <beckyp.class@gmail.com>
2015-02-21 12:06:42 -0500

    Final version of validation script

--- compareTidyDatasets.R
+++ compareTidyDatasets.R
@@ -1,5 +1,6 @@
-fileURL <- "https://s3.amazonaws.com/coursera-uploads/user-3fb4c803aacb94770fd872db/973498/asst-3/bc29cb20b5d311e4b768370a143eec8d.txt"
+fileURL <- "https://s3.amazonaws.com/coursera-uploads/user-3fb4c803aacb94770fd872db/973498/asst-3/67f5af00b56b11e4bc3477c4373a634d.txt"
 download.file(fileURL,"tidydata2.txt")
-x <- read.table("tidydata2.txt")
-y <- read.table("tidydata.txt")
-identical(x,y)
+x <- read.table("tidydata2.txt", header=TRUE)
+y <- read.table("tidydata.txt", header=TRUE)
+print(identical(x,y))
+
