494d26d296e47cec7821385a494a97ea947263f7
Becky Portier <beckyp.class@gmail.com>
2015-02-22 12:07:44 -0500

    Final version; new dataset reference

--- compareTidyDatasets.R
+++ compareTidyDatasets.R
@@ -1,4 +1,4 @@
-fileURL <- "https://s3.amazonaws.com/coursera-uploads/user-3fb4c803aacb94770fd872db/973498/asst-3/67f5af00b56b11e4bc3477c4373a634d.txt"
+fileURL <- "https://s3.amazonaws.com/coursera-uploads/user-3fb4c803aacb94770fd872db/973498/asst-3/e7c57c80baaf11e4a6e299054a240b95.txt"
 download.file(fileURL,"tidydata2.txt")
 x <- read.table("tidydata2.txt", header=TRUE)
 y <- read.table("tidydata.txt", header=TRUE)
